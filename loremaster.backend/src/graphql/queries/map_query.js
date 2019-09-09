bddcdda23051528f7020e3098912c94f10a200f0
River Marks <rmarks6767@gmail.com>
2019-09-09 12:43:25 -0400

    Got the map querying working and started to make a function for getting the properties

--- loremaster.backend/src/graphql/queries/map_query.js
+++ loremaster.backend/src/graphql/queries/map_query.js
@@ -1,17 +1,21 @@
-const { map } = require("../model/outputs/map");
+const { map } = require('../model/outputs/map');
 const { GraphQLNonNull, GraphQLString } = require('graphql'); 
-const MapRepo = require('../../repositories/map_repo')
+const { GetMapById } = require('../../repositories/map_repo');
+const { GetProperties } = require('../../extra_functions/get_properties');
 
 const mapQuery = {
     type: map,
-    description: "a map",
+    description: 'a map',
     args: {
         id: {
-            name: "id",
+            name: 'id',
             type: new GraphQLNonNull(GraphQLString)
         }
     },
-    resolve: MapRepo.GetMapById.bind(MapRepo)
+    resolve: (source, args, root, ast) => {
+        console.log(GetProperties(ast.operation.selectionSet));
+        return GetMapById(args.id );
+    }
 }
 
 module.exports = {
--- loremaster.backend/src/repositories/map_repo.js
+++ loremaster.backend/src/repositories/map_repo.js
@@ -1,89 +1,85 @@
-const { SQLCONNECT } = require('./sql_connect')
+const { SQLCONNECT } = require('../sql/sql_connect')
 const { generateUUID } = require('../extra_functions/generate_uuid')
 
 module.exports = {
-    GetMapById: function(_, { id }){
+    GetMapById: function( id ){
         const connection = SQLCONNECT()
-        var map = null;
-        if (connection){
-            connection.query(`SELECT * FROM maps RIGHT JOIN tiles ON maps.ID=tiles.map_id WHERE maps.ID="${id}"ORDER BY tiles.map_id`, function(error, result){
-                if (result){
-                    connection.end();
-                    map = {
-                        id: id,
-                        name: result[0].name,
-                        image_link: result[0].image_link,
-                        tiles: result.map(tile => {
-                            return {
-                                id: tile.ID,
-                                height: tile.height,
-                                width: tile.width,
-                                x: tile.x,
-                                y: tile.y,
-                                type: tile.type
-                            }
-                        })
-                    }
-                    
-                } else if(error){
-                    throw new Error(error);
-                } else {
-                    map = null;
-                }
-                return map;
-            });
-        } else {
-            console.log("REEEEE")
-            return null;
-        }
 
+        return new Promise((succeed, failure) => {
+            if (connection){
+                connection.query(`SELECT * FROM maps RIGHT JOIN tiles ON maps.ID=tiles.map_id WHERE maps.ID="${id}"ORDER BY tiles.map_id`, (error, result) => {
+                    if (result){
+                        return succeed({
+                            id: id,
+                            name: result[0].name,
+                            image_link: result[0].image_link,
+                            tiles: result.map(tile => {
+                                return {
+                                    id: tile.ID,
+                                    height: tile.height,
+                                    width: tile.width,
+                                    x: tile.x,
+                                    y: tile.y,
+                                    type: tile.type
+                                }
+                            })
+                        });
+                        
+                    } else if(error){
+                        return failure(error);
+                    } else {
+                        map = null;
+                    }
+                    console.log(map);
+                    return map;
+                });
+            } else {
+                console.log("REEEEE")
+                return null;
+            }
+        });
     },
     InsertMap: function(_, { map }){
-        const connection = SQLCONNECT()
-        const mapGUID = generateUUID()
+        const connection = SQLCONNECT();
+        const mapGUID = generateUUID();
 
-        //Connection success
-        if (connection){
-            connection.query(`INSERT INTO maps (ID, name, image_link) VALUES ("${mapGUID}","${map.name}","${map.image_link}")`,function(error, result){
-                if(error){
-                    console.log("Got an errorrr: " + error)
-                    return( {
-                        code: error.code,
-                        message: error.message
-                    });
-                } else {
-                    connection.end();
-                    return {
-                        code: 200,
-                        message: "OK"
-                    } 
-                }
-                console.log("Inserted map")
-            })
-            map.tiles.map(tile => {
-                const id = generateUUID(); 
-                connection.query(`INSERT INTO tiles (ID, height, width, x, y, map_id, type) VALUES ("${id}","${tile.height}","${tile.width}","${tile.x}","${tile.y}","${mapGUID}","${tile.type}")`,function(error,result){
+        return new Promise((success, failure) => {
+            //Connection success
+            if (connection){
+                connection.query(`INSERT INTO maps (ID, name, image_link) VALUES ("${mapGUID}","${map.name}","${map.image_link}")`,function(error, result){
                     if(error){
-                        console.log("Got an error: " + error)
-                        return( {
+                        console.log("Got an errorrr: " + error)
+                        return failure({
                             code: error.code,
                             message: error.message
                         });
-                    } else {
-                        connection.end();
-                        return {
-                            code: 200,
-                            message: "OK"
+                    } 
+                })
+                map.tiles.map(tile => {
+                    const id = generateUUID(); 
+                    connection.query(`INSERT INTO tiles (ID, height, width, x, y, map_id, type) VALUES ("${id}","${tile.height}","${tile.width}","${tile.x}","${tile.y}","${mapGUID}","${tile.type}")`,function(error,result){
+                        if(error){
+                            console.log("Got an error: " + error)
+                            return failure({
+                                code: error.code,
+                                message: error.message
+                            });
+                        } else {
+                            connection.end();
+                            return success({
+                                code: 200,
+                                message: "OK"
+                            })
                         }
-                    }
+                    });
                 });
-            });
-        } else {
-            console.log('Could not establish a connection')
-            return({
-                code: 409,
-                message: 'Bad Request'
-            })
-        }
+            } else {
+                console.log('Could not establish a connection')
+                return({
+                    code: 409,
+                    message: 'Bad Request'
+                })
+            }
+        });
     }
 }
