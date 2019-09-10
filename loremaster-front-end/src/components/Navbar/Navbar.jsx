dfb67b34889ccae95e452e4fce7ebff4132cbb7d
Aidan Brown (RIT Student) <afb3535@ad.rit.edu>
2019-09-10 15:12:24 -0400

    stuff

--- loremaster-front-end/src/components/Navbar/Navbar.jsx
+++ loremaster-front-end/src/components/Navbar/Navbar.jsx
@@ -7,16 +7,26 @@ class Navbar extends Component{
     render(){
         return(
             <div className="Navbar">
-                <div className="navbar navbar-header navbar-fixed-top navbar-default">
-                    <p className="nav navbar-brand">Project Loremaster</p>
+                <nav className="navbar navbar-fixed-top navbar-default">
+                    <div className="navbar-header">
+                        <p className="nav navbar-brand">Project Loremaster</p>
+                    </div>
 
-                    <ul className="nav navbar-nav nav-tabs navbar-left" role="tablist">
+                    <ul className="nav navbar-nav nav-tabs" role="tablist">
                         <li><Link to={'/home'}>Home</Link></li>
                         <li><Link to={'/characters'}>Characters</Link></li>
                         <li><Link to={'/maps'}>Maps</Link></li>
                         <li><Link to={'/campaigns'}>Campaigns</Link></li>
                         <li><Link to={'/compendium'}>Compendium</Link></li>
                     </ul>
+
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#login">Login</button></li>
+                    </ul>
+                </nav>
+                <div id="login" className="row collapse">
+                    <p>Username: <input id="username" className="input-sm"></input></p>
+                    <p>Password: <input id="password" className="input-sm"></input></p>
                 </div>
             </div>
         );
