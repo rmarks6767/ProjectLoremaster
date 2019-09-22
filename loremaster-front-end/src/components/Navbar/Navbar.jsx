9f4c4b32e3c963cf4ee94c61ad150bd15f22193a
Ryan Connors <connorsryanj@gmail.com>
2019-09-22 15:11:15 -0400

    Updates to style

--- loremaster-front-end/src/components/Navbar/Navbar.jsx
+++ loremaster-front-end/src/components/Navbar/Navbar.jsx
@@ -15,14 +15,14 @@ class Navbar extends Component{
                     </div>
 
                     <ul className="nav navbar-nav nav-tabs" role="tablist">
-                        <li><Link to={'/home'}>Home</Link></li>
-                        <li><Link to={'/characters'}>Characters</Link></li>
-                        <li><Link to={'/maps'}>Maps</Link></li>
-                        <li><Link to={'/campaigns'}>Campaigns</Link></li>
-                        <li><Link to={'/compendium'}>Compendium</Link></li>
+                        <li><Link to="/home" >Home</Link></li>
+                        <li><Link to="/characters" >Characters</Link></li>
+                        <li><Link to="/maps" >Maps</Link></li>
+                        <li><Link to="/campaigns" >Campaigns</Link></li>
+                        <li><Link to="/compendium" >Compendium</Link></li>
                     </ul>
 
-                    <button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#login">Sign In</button>
+                    <button type="button" className="btn btn-default pull-right login-button" data-toggle="modal" data-target="#login">Sign In</button>
                 </nav>
                 <Login />
             </div>
