fc782f3f9275b958d2fd2a6a0ddd1d45de569ef0
aidan-brown <abrownie99600@gmail.com>
2019-09-22 15:13:32 -0400

    Added stuff to landing

--- loremaster-front-end/src/components/Landing/Landing.jsx
+++ loremaster-front-end/src/components/Landing/Landing.jsx
@@ -12,14 +12,17 @@ class Landing extends Component{
                     <div className="row">
                         <div className="col-sm-4">
                             <h3>Characters</h3>
+                            <img className="img-thumbnail" src="https://i.redd.it/cw8sxhc3g5a01.png" alt="D&D Character Sheet"/>
                             <p>Use our character creator to create your very own D&D characters to use during campaigns</p>
                         </div>
                         <div className="col-sm-4">
                             <h3>Maps</h3>
+                            <img className="img-thumbnail" src="https://i.imgur.com/036lFAk.jpg" alt="D&D battle map" />
                             <p>Make battle and world maps using our extensive map creation tools</p>
                         </div>
                         <div className="col-sm-4">
                             <h3>Campaigns</h3>
+                            <img className="img-thumbnail" src="https://assets.atlasobscura.com/article_images/lg/60938/image.jpg" alt="D&D campaign map" />
                             <p>Make, Manage, and Play a full D&D campaign online with friends</p>
                         </div>
                     </div>
