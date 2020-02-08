7f297be3de395064259deb55fa390f0b474f70c6
Ryan Connors <connorsryanj@gmail.com>
2020-02-08 17:42:54 -0500

    Initial Create Campaign UI

--- loremaster/frontend/src/components/Pages/Campaigns/Campaigns.jsx
+++ loremaster/frontend/src/components/Pages/Campaigns/Campaigns.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import './Campaign.css';
 
 class Campaigns extends Component{
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+
+    }
+
     render(){
         return(
             <div>
                 <div className="jumbotron text-center">
-                    <h1>Campaigns</h1>
+                    <h1>Create New Campaign</h1>
+                </div>
+
+                <div className="row">
+                    <div className="maps-container col-md">
+                        <h2>Maps</h2>
+                    </div>
+                    <div className="monsters-container col-md">
+                        <h2>Monsters</h2>
+                    </div>
+                    <div className="characters-container col-md">
+                        <h2>Characters</h2>
+                    </div>
                 </div>
             </div>
         );
